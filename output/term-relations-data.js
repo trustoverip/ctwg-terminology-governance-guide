53e5fe6b5b7a02ab96a77705c52a43821aa333ea
henkvancann <hvancann@bcws.io>
2024-11-12 14:11:13 +0000

    all new files over to github too
    Signed-off-by: henkvancann <hvancann@bcws.io>

---
+++ output/term-relations-data.js
@@ -0,0 +1,58 @@
+const allTermRelations = {
+  "defs": [
+    {
+      "term": "abbrev",
+      "synonyms": [
+        "abbreviation"
+      ],
+      "refs": [
+        "term",
+        "alias"
+      ]
+    },
+    {
+      "term": "alias",
+      "synonyms": [],
+      "refs": [
+        "abbrev"
+      ]
+    },
+    {
+      "term": "doctag",
+      "synonyms": []
+    },
+    {
+      "term": "Guide",
+      "synonyms": [
+        "guide",
+        "guidelines"
+      ],
+      "refs": [
+        "normative"
+      ]
+    },
+    {
+      "term": "normative",
+      "synonyms": []
+    },
+    {
+      "term": "phrase",
+      "synonyms": [],
+      "refs": [
+        "term",
+        "abbreviation",
+        "alias",
+        "guide"
+      ]
+    },
+    {
+      "term": "term",
+      "synonyms": [],
+      "refs": [
+        "phrase",
+        "guide",
+        "abbreviation"
+      ]
+    }
+  ]
+};
